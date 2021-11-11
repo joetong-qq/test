a45a02f9cd53c833a227b1df0e231a1f5add1c4b
tongpengpeng <tongpengpeng@bytedance.com>
2021-11-11 18:41:54 +0800

    测试

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/cloudwego/kitex-examples
+
+go 1.16
+
+require (
+	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
+	github.com/apache/thrift v0.13.0
+	github.com/bytedance/gopkg v0.0.0-20210716082555-acbf5a2aa7e2
+	github.com/cloudwego/kitex v0.0.4
+	github.com/kitex-contrib/resolver-dns v0.0.0-20211026091019-d57feaffb777
+	github.com/kitex-contrib/tracer-opentracing v0.0.2
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/uber/jaeger-client-go v2.29.1+incompatible
+	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
+	go.uber.org/atomic v1.8.0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	google.golang.org/protobuf v1.26.0
+)
---
+++ hello/client/main.go
@@ -0,0 +1,44 @@
+// Copyright 2021 CloudWeGo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+	
+	"github.com/cloudwego/kitex-examples/hello/kitex_gen/api"
+	"github.com/cloudwego/kitex-examples/hello/kitex_gen/api/hello"
+	"github.com/cloudwego/kitex/client"
+	dns "github.com/kitex-contrib/resolver-dns"
+)
+
+func main() {
+	// client, err := hello.NewClient("hello", client.WithHostPorts("0.0.0.0:8888"))
+	client, err := hello.NewClient("edu-cloud-kitex-test:8888", client.WithResolver(dns.NewDNSResolver()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		req := &api.Request{Message: "my request"}
+		resp, err := client.Echo(context.Background(), req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(resp)
+		time.Sleep(time.Second)
+	}
+}
